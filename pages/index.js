cb9d8be3aa15857877796e24300fe4dd47866ec0
unknown <ks2591@gmail.com>
2020-09-07 00:04:49 +0800

    final section

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
             <div className={styles.upperMainContent}>
               <div>
                 <p className={styles.compName}>FinNexus</p>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ul</p>
+                <p style={{marginBottom: "10px"}}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ul</p>
                 <span>
                   <button className={`${styles.fnxBtn} ${styles.fnxPinkBtn}`}>Click1</button>
                   <button className={`${styles.fnxBtn} ${styles.fnxWhiteBtn}`}>Click1</button>
@@ -142,6 +142,70 @@ export default function Home() {
         </div>
       </div>
       {/** END -- Third Section Content */}
+
+      {/** START -- Subscribe section */}
+      <div className={styles.section}>
+        <div></div>
+        <div style={{ display: "flex" }}>
+          <div style={{ width: "60%", height: "100%" }}></div>
+          <div style={{ width: "40%", height: "100%", paddingTop: "15em", color: "var(--dark-pink-color)" }}>
+            <div>
+              <p style={{fontSize: "3em"}}><b>SUBSCRIBE</b></p>
+              <p>to the <b>FinNexus</b> NewsLetters</p>
+              <span style={{ position: "relative" }}>
+                <input className={styles.subscribeInput} placeholder="Your email" />
+                <button className={styles.subscribeBtn}><b>ok</b></button>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+      {/** END -- Subscribe Section */}
+
+      {/** START -- Bottom Section */}
+      <div className={styles.section}>
+        <div className={styles.leftUpperStyleBackground}></div>
+        <div className={styles.centerContentInverted}>
+          <div className={`${styles.mainContent}`} style={{ backgroundColor: "var(--top-color)", padding: "0 40px" }}>
+            <div style={{ height: "100%", width: "30%", color: "white", marginTop: "12em" }}>
+              <div style={{ marginBottom: "10px" }}>
+                <p style={{ fontSize: "3em", margin: "0", fontWeight: "900" }}>Join</p>
+                <p>the <b>FinNexus</b> community NOW</p>
+              </div>
+              <div>
+                <span className={styles.userPicIcon}></span>
+                <span className={styles.userPicIcon}></span>
+                <span className={styles.userPicIcon}></span>
+                <span className={styles.userPicIcon}></span>
+                <span className={styles.userPicIcon}></span>
+              </div>
+              <div>
+                <span className={styles.userPicIcon}></span>
+                <span className={styles.userPicIcon}></span>
+                <span className={styles.userPicIcon}></span>
+                <span className={styles.userPicIcon}></span>
+                <span className={styles.userPicIcon}></span>
+              </div>
+            </div>
+            <div style={{ height: "100%", width: "70%" }}></div>
+          </div>
+          <div className={styles.navbar}>
+            {/*https://stackoverflow.com/questions/4476526/do-i-use-img-object-or-embed-for-svg-files*/}
+            <object className={styles.logoProps} data="fnxLogo.svg" type="image/svg+xml"></object>
+            <div className={styles.navBtnsContainer}>
+              <button className={styles.fnxBtn}>Click1</button>
+              <button className={styles.fnxBtn}>Click1</button>
+              <button className={styles.fnxBtn}>Click1</button>
+              <button className={styles.fnxBtn}>Click1</button>
+              <button className={styles.fnxBtn}>Click1</button>
+            </div>
+          </div>
+          <div style={{ backgroundColor: "var(--top-color)", textAlign: "center", color: "white" }}>Copyright etc...</div>
+        </div>
+        <div className={styles.rightUpperStyleBackground}></div>
+      </div>
+      {/** END -- Bottom Section */}
     </div>
   );
 }
