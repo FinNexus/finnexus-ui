36dfaa987ee2d936188766217fc07fc081aba25b
unknown <ks2591@gmail.com>
2020-09-13 22:23:24 +0800

    responsive addendum

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,8 @@ import styles from "../styles/Home.module.css";
 import Head from 'next/head';
 import ArrowComponent from '../components/arrow';
 import MemberComponent from '../components/member';
-import { Container } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
+import { Container, Grid, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   root: {
@@ -47,7 +45,7 @@ export default function Home() {
             <Grid item className={styles.upperMainContent}>
               <Grid lg={7} item>
                 <p className={styles.compName}>FinNexus</p>
-                <p style={{ marginBottom: "10px" }}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ul</p>
+                <p className={styles.contentUnderImg} style={{ marginBottom: "10px" }}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ul</p>
                 <span>
                   <button className={`${styles.fnxBtn} ${styles.fnxPinkBtn}`}>Click1</button>
                   <button className={`${styles.fnxBtn} ${styles.fnxWhiteBtn}`}>Click1</button>
@@ -64,81 +62,51 @@ export default function Home() {
 
       {/** START -- Next Section Content */}
       <Container maxWidth="lg" className={`${styles.section} ${styles.middleSection}`} style={{ maxWidth: "2000px" }}>
-        <div className={`${styles.middleSectionContent}`}>
-          <div className={`${styles.halfMiddleSection}`}>
-            <div className={styles.halfMiddleOneThird}>
-              <div className={styles.imageContainer}>
+        <Container maxWidth="lg" className={`${styles.mainContent}`}>
+          <Container maxWidth="lg" className={`${styles.halfMiddleSection}`} style={{ display: "flex" }}>
+            <Grid item lg={4} className={styles.halfMiddleOneThird}>
+              <Grid item md={7} className={styles.imageContainer}>
                 <div className={styles.imageBox}></div>
                 <div className={styles.textBox}><span className={styles.biggerText}>FPO</span> v1.0</div>
-              </div>
-              <div style={{ padding: "30px 0", color: "white" }}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ul
+              </Grid>
+              <div className={styles.contentUnderImg} style={{ padding: "30px 0", color: "white" }}><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ul</p>
               </div>
               <button className={`${styles.fnxBtn} ${styles.fnxWhiteBtn}`}>Click1</button>
-            </div>
-            <div className={styles.halfMiddleTwoThird}></div>
-          </div>
-        </div>
+            </Grid>
+            <Grid item lg={7} className={styles.halfMiddleTwoThird}></Grid>
+          </Container>
+        </Container>
       </Container>
       {/** END -- Next Section Content */}
 
       {/** START -- Third Section Content */}
-      <div className={`${styles.section} ${styles.thirdSection}`}>
-        <div className={styles.leftThirdSection}>
-          <div style={{ height: "25%" }}></div>
-          <div className={styles.leftThirdOuterSection}><div className={styles.leftThirdInnerSection}></div></div>
-        </div>
-        <div className={styles.thirdSectionContent}>
-          <div className={styles.picBoxContainer}>
-            <div className={styles.picBox}>
+      <Container maxWidth="lg" className={`${styles.section} ${styles.thirdSection}`} style={{ maxWidth: "2000px" }}>
+        <Container maxWidth="lg" className={styles.thirdSectionContent}>
+          <Container maxWidth="lg" className={styles.picBoxContainer} style={{ display: "flex" }}>
+            <Grid lg={3} sm={3} className={`${styles.picBox}`} spacing={2}>
               <div className={styles.innerPicBox}>
                 <p className={styles.innerBoxTitle}>Title</p>
                 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy </p>
                 <ArrowComponent position="bottom right" size="medium" glyphDir="right" glyphColor="pink" />
               </div>
-            </div>
-            <div className={styles.picBox}>
+            </Grid>
+            <Grid lg={3} sm={3} className={styles.picBox} spacing={2}>
               <div className={styles.innerPicBox}>
                 <p className={styles.innerBoxTitle}>Title</p>
                 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy </p>
                 <ArrowComponent position="bottom right" size="medium" glyphDir="right" glyphColor="pink" />
               </div>
-            </div>
-            <div className={styles.picBox}>
+            </Grid>
+            <Grid lg={3} sm={3} className={styles.picBox} spacing={2}>
               <div className={styles.innerPicBox}>
                 <p className={styles.innerBoxTitle}>Title</p>
                 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy </p>
                 <ArrowComponent position="bottom right" size="medium" glyphDir="right" glyphColor="pink" />
               </div>
-            </div>
-          </div>
-          {/*  <div className={styles.picSliderContainer}>
-            <div className={styles.picSliderTextBox}><div style={{ width: "25%" }}></div><div><p className={styles.picSliderTextHeader}>INVESTORS</p><p className={styles.picSliderTextContent}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy</p></div></div>
-            <div className={styles.picSlider}>
-              <span><ArrowComponent right="94%" bottom="96px" size="large" glyphDir="left" glyphColor="blue" /></span>
-              <div className={styles.picSliderBox}></div>
-              <div className={styles.picSliderBox}></div>
-              <div className={styles.picSliderBox}></div>
-              <div className={styles.picSliderBox}></div>
-              <span><ArrowComponent right="63px" bottom="91px" size="large" glyphDir="right" glyphColor="blue" /></span>
-            </div>
-          </div>
-          <div className={styles.picSliderContainer}>
-            <div className={styles.picSliderTextBox}><div style={{ width: "25%" }}></div><div><p className={styles.picSliderTextHeader}>INVESTORS</p><p className={styles.picSliderTextContent}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy</p></div></div>
-            <div className={styles.picSlider}>
-              <span><ArrowComponent right="94%" bottom="96px" size="large" glyphDir="left" glyphColor="lightpink" /></span>
-              <div className={styles.picSliderBox}></div>
-              <div className={styles.picSliderBox}></div>
-              <div className={styles.picSliderBox}></div>
-              <div className={styles.picSliderBox}></div>
-              <span><ArrowComponent right="63px" bottom="91px" size="large" glyphDir="right" glyphColor="lightpink" /></span>
-            </div>
-          </div> */}
-        </div>
-        <div className={styles.rightThirdSection}>
-          <div style={{ height: "25%" }}></div>
-          <div className={styles.rightThirdOuterSection}><div className={styles.rightThirdInnerSection}></div></div>
-        </div>
-      </div>
+            </Grid>
+          </Container>
+        </Container>
+      </Container>
       {/** END -- Third Section Content */}
 
       <div className={`${styles.section}`}>
